66f897b2cc8bdcb2b5eb030947e3ac8330d80962
PyFulken <64746679+PyFulken@users.noreply.github.com>
2020-07-26 18:02:04 +0100

    Query testing
    Simple Mongo query testing!

--- database_manipulation.py
+++ database_manipulation.py
@@ -5,12 +5,21 @@ from mongo_connector import connection_set
 db = connection_set(False, "python-database")
 
 # Create x amount of documents to insert using code done in create_data.py
-python_document = create_documents(10)
+# python_document = create_documents(10)
 
 # Insert documents:
 # --------WARNING--------
 # The name between db. and .insert is the collection name to insert into.
 # If it doesn't exist, it will be created, misplacing the documents.
-#db.python_collection_2.insert_many(python_document)
+# db.python_collection_2.insert_many(python_document)
 
 # Search queries:
+# --------WARNING--------
+# To receive and handle collections they must be firstly instanced with:
+# correct_collection_name = db["desired_collection_name"]
+python_collection = db["python_collection"]
+# For deeper and finer searches a query must be passed as the first argument in find(). Queries are dictionaries.
+query = {"_id": {"$lte": 10}}
+received_document = python_collection.find(query)
+for i in received_document:
+    print(i)
